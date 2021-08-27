db7de058df36b20670e5a5251ab678e48cd54c04
glk1001 <gregg.kay@gmail.com>
2021-08-27 17:36:48 +1000

    Cleaned plugin_info.

--- lib/goom/include/goom/goom_plugin_info.h
+++ lib/goom/include/goom/goom_plugin_info.h
@@ -21,10 +21,10 @@ public:
   };
 
   PluginInfo() noexcept = delete;
-  virtual ~PluginInfo() noexcept = default;
   PluginInfo(uint32_t width, uint32_t height) noexcept;
-  PluginInfo(const PluginInfo& p) noexcept;
+  PluginInfo(const PluginInfo& p) noexcept = default;
   PluginInfo(PluginInfo&&) noexcept = delete;
+  virtual ~PluginInfo() noexcept = default;
   auto operator=(const PluginInfo&) -> PluginInfo& = delete;
   auto operator=(PluginInfo&&) -> PluginInfo& = delete;
 
@@ -36,7 +36,7 @@ protected:
 
 private:
   const Screen m_screen;
-  const std::unique_ptr<SoundInfo> m_soundInfo;
+  std::unique_ptr<SoundInfo> m_soundInfo;
 };
 
 class WritablePluginInfo : public PluginInfo
@@ -48,11 +48,6 @@ public:
 
 inline PluginInfo::PluginInfo(const uint32_t width, const uint32_t height) noexcept
   : m_screen{width, height, width * height}, m_soundInfo{std::make_unique<SoundInfo>()}
-{
-}
-
-inline PluginInfo::PluginInfo(const PluginInfo& p) noexcept
-  : m_screen{p.m_screen}, m_soundInfo{new SoundInfo{*p.m_soundInfo}}
 {
 }
 
