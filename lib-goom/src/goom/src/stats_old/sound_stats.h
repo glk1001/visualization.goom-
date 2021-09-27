01615c1c71a6d1f006c97c2516d1ab0e68dab52c
glk1001 <gregg.kay@gmail.com>
2021-09-27 18:47:01 +1000

    cmake - Split goom and libs into separate cmake sub-project 'lib-goom'.

---
+++ lib-goom/src/goom/src/stats_old/sound_stats.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "goom/goom_config.h"
+#include "goom/goom_stats.h"
+
+#include <cstdint>
+
+namespace GOOM
+{
+
+class SoundStats
+{
+public:
+  SoundStats() = default;
+
+  void Reset();
+  void Log(const GoomStats::LogStatsValueFunc& logVal) const;
+
+  void LastGoomPower(float val);
+
+  void DoGoom();
+  void DoBigGoom();
+  void DoUpdateVolume(float volume);
+  void DoUpdateAcceleration(float acceleration);
+  void DoUpdateSpeed(float speed);
+
+private:
+  uint64_t m_numGooms = 0;
+  uint64_t m_numBigGooms = 0;
+
+  float m_lastGoomPower = 0.0;
+
+  float m_minVolume = 1.0;
+  float m_maxVolume = 0.0;
+  double m_totalVolume = 0.0;
+  uint64_t m_numVolumeUpdates = 0;
+
+  float m_minAcceleration = 1.0;
+  float m_maxAcceleration = 0.0;
+  double m_totalAcceleration = 0.0;
+  uint64_t m_numAccelerationUpdates = 0;
+
+  float m_minSpeed = 1.0;
+  float m_maxSpeed = 0.0;
+  double m_totalSpeed = 0.0;
+  uint64_t m_numSpeedUpdates = 0;
+};
+
+} // namespace GOOM
+
