a2aa62a9b3b0f97e362cfcaa890e3ad5b9fe29fc
glk1001 <gregg.kay@gmail.com>
2021-10-24 20:06:27 +1100

    Added more tone maps.

--- visualization.goom/resources/shaders/GL/fragment.glsl
+++ visualization.goom/resources/shaders/GL/fragment.glsl
@@ -54,6 +54,156 @@ float tonemapFilmic(float x) {
   return pow(result, 2.2);
 }
 
+// Lottes 2016, "Advanced Techniques and Optimization of HDR Color Pipelines"
+vec3 lottes(vec3 x) {
+  const vec3 a = vec3(1.6);
+  const vec3 d = vec3(0.977);
+  const vec3 hdrMax = vec3(8.0);
+  const vec3 midIn = vec3(0.18);
+  const vec3 midOut = vec3(0.267);
+
+  const vec3 b =
+      (-pow(midIn, a) + pow(hdrMax, a) * midOut) /
+      ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
+  const vec3 c =
+      (pow(hdrMax, a * d) * pow(midIn, a) - pow(hdrMax, a) * pow(midIn, a * d) * midOut) /
+      ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
+
+  return pow(x, a) / (pow(x, a * d) * b + c);
+}
+
+float lottes(float x) {
+  const float a = 1.6;
+  const float d = 0.977;
+  const float hdrMax = 8.0;
+  const float midIn = 0.18;
+  const float midOut = 0.267;
+
+  const float b =
+      (-pow(midIn, a) + pow(hdrMax, a) * midOut) /
+      ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
+  const float c =
+      (pow(hdrMax, a * d) * pow(midIn, a) - pow(hdrMax, a) * pow(midIn, a * d) * midOut) /
+      ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
+
+  return pow(x, a) / (pow(x, a * d) * b + c);
+}
+
+// Uchimura 2017, "HDR theory and practice"
+// Math: https://www.desmos.com/calculator/gslcdxvipg
+// Source: https://www.slideshare.net/nikuque/hdr-theory-and-practicce-jp
+vec3 uchimura(vec3 x, float P, float a, float m, float l, float c, float b) {
+  float l0 = ((P - m) * l) / a;
+  float L0 = m - m / a;
+  float L1 = m + (1.0 - m) / a;
+  float S0 = m + l0;
+  float S1 = m + a * l0;
+  float C2 = (a * P) / (P - S1);
+  float CP = -C2 / P;
+
+  vec3 w0 = vec3(1.0 - smoothstep(0.0, m, x));
+  vec3 w2 = vec3(step(m + l0, x));
+  vec3 w1 = vec3(1.0 - w0 - w2);
+
+  vec3 T = vec3(m * pow(x / m, vec3(c)) + b);
+  vec3 S = vec3(P - (P - S1) * exp(CP * (x - S0)));
+  vec3 L = vec3(m + a * (x - m));
+
+  return T * w0 + L * w1 + S * w2;
+}
+
+vec3 uchimura(vec3 x) {
+  const float P = 1.0;  // max display brightness
+  const float a = 1.0;  // contrast
+  const float m = 0.22; // linear section start
+  const float l = 0.4;  // linear section length
+  const float c = 1.33; // black
+  const float b = 0.0;  // pedestal
+
+  return uchimura(x, P, a, m, l, c, b);
+}
+
+float uchimura(float x, float P, float a, float m, float l, float c, float b) {
+  float l0 = ((P - m) * l) / a;
+  float L0 = m - m / a;
+  float L1 = m + (1.0 - m) / a;
+  float S0 = m + l0;
+  float S1 = m + a * l0;
+  float C2 = (a * P) / (P - S1);
+  float CP = -C2 / P;
+
+  float w0 = 1.0 - smoothstep(0.0, m, x);
+  float w2 = step(m + l0, x);
+  float w1 = 1.0 - w0 - w2;
+
+  float T = m * pow(x / m, c) + b;
+  float S = P - (P - S1) * exp(CP * (x - S0));
+  float L = m + a * (x - m);
+
+  return T * w0 + L * w1 + S * w2;
+}
+
+float uchimura(float x) {
+  const float P = 1.0;  // max display brightness
+  const float a = 1.0;  // contrast
+  const float m = 0.22; // linear section start
+  const float l = 0.4;  // linear section length
+  const float c = 1.33; // black
+  const float b = 0.0;  // pedestal
+
+  return uchimura(x, P, a, m, l, c, b);
+}
+
+// Uncharted
+vec3 uncharted2Tonemap(vec3 x) {
+  float A = 0.15;
+  float B = 0.50;
+  float C = 0.10;
+  float D = 0.20;
+  float E = 0.02;
+  float F = 0.30;
+  float W = 11.2;
+  return ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F;
+}
+
+vec3 uncharted2(vec3 color) {
+  const float W = 11.2;
+  float exposureBias = 2.0;
+  vec3 curr = uncharted2Tonemap(exposureBias * color);
+  vec3 whiteScale = 1.0 / uncharted2Tonemap(vec3(W));
+  return curr * whiteScale;
+}
+
+float uncharted2Tonemap(float x) {
+  float A = 0.15;
+  float B = 0.50;
+  float C = 0.10;
+  float D = 0.20;
+  float E = 0.02;
+  float F = 0.30;
+  float W = 11.2;
+  return ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F;
+}
+
+float uncharted2(float color) {
+  const float W = 11.2;
+  const float exposureBias = 2.0;
+  float curr = uncharted2Tonemap(exposureBias * color);
+  float whiteScale = 1.0 / uncharted2Tonemap(W);
+  return curr * whiteScale;
+}
+
+// Unreal 3, Documentation: "Color Grading"
+// Adapted to be close to Tonemap_ACES, with similar range
+// Gamma 2.2 correction is baked in, don't use with sRGB conversion!
+vec3 unreal(vec3 x) {
+  return x / (x + 0.155) * 1.019;
+}
+
+float unreal(float x) {
+  return x / (x + 0.155) * 1.019;
+}
+
 // ===============================================
 out vec4 fragColor;
 
@@ -64,16 +214,23 @@ void main()
 {
   vec3 hdrColor = texture(texBuffer, texCoords).rgb;
 
-  // tone mapping
+  // Tone mapping
+  //vec3 mapped = hdrColor;
+  //vec3 mapped = reinhard(hdrColor);
   //vec3 mapped = reinhard2(hdrColor);
-  vec3 mapped = tonemapFilmic(hdrColor);
+  //vec3 mapped = tonemapFilmic(hdrColor); // Doesn't seem to show anything
+  //vec3 mapped = aces(hdrColor);
+  vec3 mapped = lottes(hdrColor);
+  //vec3 mapped = uchimura(hdrColor);
+  //vec3 mapped = uncharted2Tonemap(hdrColor);
+  //vec3 mapped = unreal(hdrColor); // needs lower 'A' value
 
-  // exposure tone mapping
-//  const float exposure = 2.0;\n"
-//  vec3 mapped = vec3(1.0) - exp(-hdrColor * exposure);\n"
+  // Exposure tone mapping
+  //const float exposure = 2.0;\n"
+  //vec3 mapped = vec3(1.0) - exp(-hdrColor * exposure);\n"
 
-  // gamma correction
-  const float A = 11.0;
+  // Gamma correction
+  const float A = 30.0;
   const float gamma = 2.2;
   mapped = A * pow(mapped, vec3(1.0 / gamma));
 
