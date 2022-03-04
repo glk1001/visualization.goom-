7a45ee98d8c9beaeb01ea6e2cdac92ed846db830
glk1001 <gregg.kay@gmail.com>
2022-03-05 09:55:22 +1100

    More aggressive image velocity effect.

--- depends/goom-libs/src/goom/src/visual_fx/filters/image_velocity.cpp
+++ depends/goom-libs/src/goom/src/visual_fx/filters/image_velocity.cpp
@@ -11,9 +11,9 @@ using UTILS::GetFullParamGroup;
 using UTILS::NameValuePairs;
 using UTILS::MATH::IGoomRand;
 
-static constexpr IGoomRand::NumberRange<float> AMPLITUDE_RANGE = {0.0025F, 0.0100F};
+static constexpr IGoomRand::NumberRange<float> AMPLITUDE_RANGE = {0.0025F, 0.0500F};
 static constexpr IGoomRand::NumberRange<float> COLOR_CUTOFF_RANGE = {0.1F, 0.9F};
-static constexpr IGoomRand::NumberRange<float> ZOOM_FACTOR_RANGE = {0.9F, 1.0F};
+static constexpr IGoomRand::NumberRange<float> ZOOM_FACTOR_RANGE = {0.5F, 1.0F};
 
 static constexpr float PROB_XY_COLOR_CUTOFFS_EQUAL = 0.5F;
 
