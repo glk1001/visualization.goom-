0f91efd9a32949862b75efc0c2abd07fe4626dce
glk1001 <gregg.kay@gmail.com>
2022-05-01 12:02:30 +1000

    Added 'tee' core command to sudoers.

--- goom-docker-run.sh
+++ goom-docker-run.sh
@@ -11,20 +11,26 @@ declare -r MUSIC_SHARE="/mnt/Music"
 declare -r OLD_CORE_PATTERN=$(cat /proc/sys/kernel/core_pattern)
 echo
 echo "Setting kodi 'core' pattern:"
-echo '/tmp/core.%e.%p' | sudo tee /proc/sys/kernel/core_pattern
+echo '/tmp/core.%e.%p' | sudo /usr/bin/tee /proc/sys/kernel/core_pattern
 
 echo
-x11docker --runasroot="service lircd start" --network --pulseaudio --gpu \
-          --share=${MUSIC_SHARE} --home=${CONTAINER_HOME_DIR} --name ${CONTAINER_NAME} \
+x11docker --runasroot="service lircd start" \
+		  --name ${CONTAINER_NAME} \
+          --hostuser=greg \
+		  --network \
+		  --pulseaudio \
+		  --gpu \
+          --share=${MUSIC_SHARE} \
+          --home=${CONTAINER_HOME_DIR} \
           -- \
-	  --init \
-	  --privileged \
-	  --ulimit core=-1 \
-	  --mount type=bind,source=/tmp/,target=/tmp/ \
-	  -- \
-	  ${IMAGE_NAME}
+          --init \
+		  --privileged \
+          --ulimit core=-1 \
+          --mount type=bind,source=/tmp/,target=/tmp/ \
+          -- \
+	      ${IMAGE_NAME}
 
 echo "x11docker return code = $?"
 
-echo "${OLD_CORE_PATTERN}" | sudo tee /proc/sys/kernel/core_pattern > /dev/null
+echo "${OLD_CORE_PATTERN}" | sudo /usr/bin/tee /proc/sys/kernel/core_pattern > /dev/null
 
