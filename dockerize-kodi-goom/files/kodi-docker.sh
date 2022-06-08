7e2f11a06586518517ddd502af0238ca8a1dd2c6
glk1001 <gregg.kay@gmail.com>
2022-06-08 17:05:38 +1000

    Aded core pattern to kodi-docker.sh

--- dockerize-kodi-goom/files/kodi-docker.sh
+++ dockerize-kodi-goom/files/kodi-docker.sh
@@ -10,14 +10,14 @@ declare -r CONTAINER_LOG_DIR="${CONTAINER_HOME_DIR}/kodi-log"
 declare -r CONTAINER_LOG="${CONTAINER_LOG_DIR}/kodi-docker.log"
 declare -r MUSIC_SHARE="/mnt/Music"
 declare -r VIDEO_SHARE="/mnt/Video"
-declare -r UB_VIDEO_SHARE="/mnt/ub-video"
+declare -r EXTRA_VIDEO_SHARE="/mnt/ub-video"
 
 declare ALL_SHARES="--share=${MUSIC_SHARE} --share=${VIDEO_SHARE}"
 
 
 declare -r IREXEC_CAPTURES=${CONTAINER_HOME_DIR}/temp/irexec.out
-if [[ -d "${UB_VIDEO_SHARE}" ]]; then
-    ALL_SHARES="${ALL_SHARES} --share=${UB_VIDEO_SHARE}"
+if [[ -d "${EXTRA_VIDEO_SHARE}" ]]; then
+    ALL_SHARES="${ALL_SHARES} --share=${EXTRA_VIDEO_SHARE}"
 fi
 
 rm -f "${IREXEC_CAPTURES}" |& tee -a "${CONTAINER_LOG}"
@@ -27,11 +27,24 @@ declare -r CORE_PATTERN="/tmp/core.%e.%p"
 ulimit -c unlimited
 sudo /usr/sbin/sysctl -q -w kernel.core_pattern="${CORE_PATTERN}"
 
+
 x11docker --runasroot="service lircd start" ${ALL_SHARES} \
-          --hostuser=greg --network --pulseaudio --gpu \
-          --home=${CONTAINER_HOME_DIR} --name ${CONTAINER_NAME} \
-          -- --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged \
-          -- ${IMAGE_NAME} |& tee -a "${CONTAINER_LOG}"
+          --hostuser=${USER}                              \
+		  --network                                       \
+		  --pulseaudio                                    \
+		  --gpu                                           \
+          --home=${CONTAINER_HOME_DIR}                    \
+		  --name ${CONTAINER_NAME}                        \
+          --                                              \
+          --init                                          \
+		  --cap-add=SYS_PTRACE                            \
+		  --security-opt seccomp=unconfined               \
+		  --privileged                                    \
+          --ulimit core=-1                                \
+          --mount type=bind,source=/tmp/,target=/tmp/     \
+          --                                              \
+		  ${IMAGE_NAME} |& tee -a "${CONTAINER_LOG}"
+
 
 if grep KEY_SLEEP "${IREXEC_CAPTURES}" > /dev/null ; then
     >&2 echo "Power off requested. Shutting down now..." |& tee -a "${CONTAINER_LOG}"
