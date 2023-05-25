bb021b8c05d7f6e267ebd88b11bc69ee007410cb
glk1001 <gregg.kay@gmail.com>
2023-05-25 21:27:09 +1000

    Utils: Added file include expander.

--- depends/goom-libs/src/goom/src/utils/strutils.cpp
+++ depends/goom-libs/src/goom/src/utils/strutils.cpp
@@ -3,11 +3,133 @@
 #if __cplusplus > 201703L
 #include <ranges>
 #endif
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 namespace GOOM::UTILS
 {
+
+namespace
+{
+
+// NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
+auto GetIncludeFileName(const std::string& includeDir, const std::string& includeLine)
+    -> std::string
+{
+  const auto words = StringSplit(includeLine, " ");
+  if (words.size() <= 1)
+  {
+    throw std::runtime_error("Expected filename after #include");
+  }
+  const auto& filename = words.at(1);
+  if (filename.size() < 2)
+  {
+    throw std::runtime_error("#include filename is not double quoted");
+  }
+  if (filename[0] != '\"')
+  {
+    throw std::runtime_error(
+        std_fmt::format("Expected #include filename '{}' to start with \"", filename));
+  }
+  if (filename[filename.size() - 1] != '\"')
+  {
+    throw std::runtime_error(
+        std_fmt::format("Expected #include filename '{}' to end with \"", filename));
+  }
+  if (filename.size() <= 2)
+  {
+    throw std::runtime_error("#include filename is empty");
+  }
+  return includeDir + "/" + filename.substr(1, filename.size() - 2);
+}
+
+} // namespace
+
+auto GetFileLines(const std::string& filepath) -> std::vector<std::string>
+{
+  auto inStream = std::ifstream{filepath};
+  if (not inStream)
+  {
+    throw std::runtime_error{std_fmt::format("Could not open file \"{}\"", filepath)};
+  }
+
+  return GetFileLines(inStream);
+}
+
+auto GetFileLines(std::istream& inStream) -> std::vector<std::string>
+{
+  auto lines = std::vector<std::string>{};
+  while (true)
+  {
+    auto line = std::string{};
+    getline(inStream, line);
+    if (inStream.eof())
+    {
+      break;
+    }
+    lines.push_back(line);
+  }
+  return lines;
+}
+
+auto PutFileLines(const std::string& filepath, const std::vector<std::string>& lines) -> void
+{
+  auto outStream = std::ofstream{filepath};
+  if (not outStream)
+  {
+    throw std::runtime_error{std_fmt::format("Could not open file \"{}\"", filepath)};
+  }
+
+  PutFileLines(outStream, lines);
+}
+
+auto PutFileLines(std::ostream& outStream, const std::vector<std::string>& lines) -> void
+{
+  for (auto i = 0U; i < lines.size(); ++i)
+  {
+    outStream << lines.at(i) << "\n";
+  }
+}
+
+auto GetFileLinesWithExpandedIncludes(const std::string& includeDir, const std::string& filepath)
+    -> std::vector<std::string>
+{
+  return GetFileLinesWithExpandedIncludes(includeDir, GetFileLines(filepath));
+}
+
+auto GetFileLinesWithExpandedIncludes(const std::string& includeDir,
+                                      const std::vector<std::string>& inLines)
+    -> std::vector<std::string>
+{
+  if (not std::filesystem::exists(includeDir))
+  {
+    throw std::runtime_error{
+        std_fmt::format("Could not find include directory \"{}\"", includeDir)};
+  }
+
+  auto outLines = std::vector<std::string>{};
+  for (const auto& line : inLines)
+  {
+    const auto trimmedLine = RTrimAndCopy(line);
+
+    if (static constexpr auto INCLUDE = std::string_view{"#include"};
+        trimmedLine.substr(0, INCLUDE.size()) != INCLUDE)
+    {
+      outLines.push_back(line);
+    }
+    else
+    {
+      const auto includeLines = GetFileLines(GetIncludeFileName(includeDir, trimmedLine));
+      std::copy(cbegin(includeLines), cend(includeLines), std::back_inserter(outLines));
+    }
+  }
+
+  return outLines;
+}
 
 auto FindAndReplaceAll(std::string& dataStr,
                        const std::string& searchStr,
--- depends/goom-libs/src/goom/src/utils/strutils.h
+++ depends/goom-libs/src/goom/src/utils/strutils.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <string>
 #include <string_view>
 #include <vector>
 
@@ -18,5 +20,64 @@ auto FindAndReplaceAll(std::string& dataStr,
 
 [[nodiscard]] auto StringJoin(const std::vector<std::string>& strings,
                               const std::string_view& delim) -> std::string;
+
+auto LTrim(std::string& str) noexcept -> void;
+auto RTrim(std::string& str) noexcept -> void;
+auto Trim(std::string& str) noexcept -> void;
+[[nodiscard]] auto LTrimAndCopy(std::string str) noexcept -> std::string;
+[[nodiscard]] auto RTrimAndCopy(std::string str) noexcept -> std::string;
+[[nodiscard]] auto TrimAndCopy(std::string str) noexcept -> std::string;
+
+[[nodiscard]] auto GetFileLines(const std::string& filepath) -> std::vector<std::string>;
+[[nodiscard]] auto GetFileLines(std::istream& inStream) -> std::vector<std::string>;
+
+auto PutFileLines(const std::string& filepath, const std::vector<std::string>& lines) -> void;
+auto PutFileLines(std::ostream& outStream, const std::vector<std::string>& lines) -> void;
+
+[[nodiscard]] auto GetFileLinesWithExpandedIncludes(const std::string& includeDir,
+                                                    const std::string& filepath)
+    -> std::vector<std::string>;
+[[nodiscard]] auto GetFileLinesWithExpandedIncludes(const std::string& includeDir,
+                                                    const std::vector<std::string>& inLines)
+    -> std::vector<std::string>;
+
+inline auto LTrim(std::string& str) noexcept -> void
+{
+  str.erase(str.begin(),
+            std::find_if(
+                str.begin(), str.end(), [](unsigned char chr) { return 0 == std::isspace(chr); }));
+}
+
+inline auto RTrim(std::string& str) noexcept -> void
+{
+  str.erase(std::find_if(
+                str.rbegin(), str.rend(), [](unsigned char chr) { return 0 == std::isspace(chr); })
+                .base(),
+            str.end());
+}
+
+inline auto Trim(std::string& str) noexcept -> void
+{
+  RTrim(str);
+  LTrim(str);
+}
+
+inline auto LTrimAndCopy(std::string str) noexcept -> std::string
+{
+  LTrim(str);
+  return str;
+}
+
+inline auto RTrimAndCopy(std::string str) noexcept -> std::string
+{
+  RTrim(str);
+  return str;
+}
+
+inline auto TrimAndCopy(std::string str) noexcept -> std::string
+{
+  Trim(str);
+  return str;
+}
 
 } // namespace GOOM::UTILS
