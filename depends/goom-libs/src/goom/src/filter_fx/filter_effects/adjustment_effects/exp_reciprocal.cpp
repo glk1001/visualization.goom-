af85bd841785d4140caa3798c12fcdf028cd5bf2
glk1001 <gregg.kay@gmail.com>
2024-08-19 19:47:48 +1000

    ExpReciprocal: Use a max z test to avoid 'inf' values.

--- depends/goom-libs/src/goom/src/filter_fx/filter_effects/adjustment_effects/exp_reciprocal.cpp
+++ depends/goom-libs/src/goom/src/filter_fx/filter_effects/adjustment_effects/exp_reciprocal.cpp
@@ -1,10 +1,12 @@
 module;
 
 //#undef NO_LOGGING
+//#include "goom/goom_logger.h"
 
 #include <cmath>
 #include <complex>
 #include <cstdint>
+#include <numeric>
 
 module Goom.FilterFx.FilterEffects.AdjustmentEffects.ExpReciprocal;
 
@@ -15,6 +17,7 @@ import Goom.Utils.NameValuePairs;
 import Goom.Utils.Math.GoomRand;
 import Goom.Utils.Math.Misc;
 import Goom.Lib.Point2d;
+//import Goom.Utils.DebuggingLogger;
 
 namespace GOOM::FILTER_FX::FILTER_EFFECTS
 {
@@ -29,29 +32,42 @@ using UTILS::MATH::NumberRange;
 using UTILS::MATH::SMALL_FLOAT;
 using UTILS::MATH::TWO_PI;
 
-static constexpr auto AMPLITUDE_RANGE           = NumberRange{0.01F, 0.11F};
-static constexpr auto LERP_TO_ONE_T_RANGE       = NumberRange{0.0F, 1.0F};
+static constexpr auto MAX_DOUBLE = std::numeric_limits<double>::max();
+
+static constexpr auto AMPLITUDE_RANGE           = NumberRange{0.01F, 0.2F};
+static constexpr auto LERP_TO_ONE_T_RANGE       = NumberRange{0.1F, 1.0F};
 static constexpr auto ROTATE_RANGE              = NumberRange{0.0F, TWO_PI};
 static constexpr auto MAGNIFY_RANGE             = NumberRange{0.95F, 1.05F};
 static constexpr auto RECIPROCAL_EXPONENT_RANGE = NumberRange{3U, 6U};
 static constexpr auto MODULATOR_PERIOD_RANGE    = NumberRange{1.0F, 100.0F};
+static constexpr auto MAX_ABS_SQ_FZ_RANGE       = NumberRange{1.0e-300 * MAX_DOUBLE, MAX_DOUBLE};
 
+static constexpr auto MAX_VIEWPORT_COORD = 6.0F;
+static constexpr auto MIN_VIEWPORT_COORD = -MAX_VIEWPORT_COORD;
+static constexpr auto MAX_SIDE_LEN       = MAX_VIEWPORT_COORD - MIN_VIEWPORT_COORD;
+static_assert(MIN_VIEWPORT_COORD < 1.0F);
+// clang-format off
 static constexpr auto VIEWPORT_BOUNDS = RandomViewport::Bounds{
     .minSideLength       = 0.1F,
     .probUseCentredSides = 0.5F,
-    .rect                = {.minMaxXMin = {.minValue = -10.0F, .maxValue = +1.0F},
-                            .minMaxYMin = {.minValue = -10.0F, .maxValue = +1.0F},
-                            .minMaxXMax = {.minValue = -10.0F + 0.1F, .maxValue = +10.0F},
-                            .minMaxYMax = {.minValue = -10.0F + 0.1F, .maxValue = +10.0F}},
-    .sides               = {.minMaxWidth  = {.minValue = 0.1F, .maxValue = 20.0F},
-                            .minMaxHeight = {.minValue = 0.1F, .maxValue = 20.0F}}
+    .rect                = {.minMaxXMin = {.minValue = MIN_VIEWPORT_COORD, .maxValue = +1.0F},
+                            .minMaxYMin = {.minValue = MIN_VIEWPORT_COORD, .maxValue = +1.0F},
+                            .minMaxXMax = {.minValue = MIN_VIEWPORT_COORD + 0.1F,
+                                           .maxValue = MAX_VIEWPORT_COORD},
+                            .minMaxYMax = {.minValue = MIN_VIEWPORT_COORD + 0.1F,
+                                           .maxValue = MAX_VIEWPORT_COORD}},
+    .sides               = {.minMaxWidth  = {.minValue = 0.1F, .maxValue = MAX_SIDE_LEN},
+                            .minMaxHeight = {.minValue = 0.1F, .maxValue = MAX_SIDE_LEN}}
 };
+// clang-format on
 
 static constexpr auto PROB_AMPLITUDES_EQUAL         = 0.95F;
 static constexpr auto PROB_LERP_TO_ONE_T_S_EQUAL    = 0.95F;
 static constexpr auto PROB_NO_INVERSE_SQUARE        = 0.50F;
 static constexpr auto PROB_USE_NORMALIZED_AMPLITUDE = 0.50F;
 static constexpr auto PROB_USE_MODULATOR_CONTOURS   = 0.10F;
+static constexpr auto PROB_USE_MAX_ABS_SQ_FZ        = 0.10F;
+static constexpr auto PROB_USE_RANDOM_VIEWPORT      = 0.80F;
 
 ExpReciprocal::ExpReciprocal(const GoomRand& goomRand) noexcept
   : m_goomRand{&goomRand}, m_randomViewport{goomRand, VIEWPORT_BOUNDS}, m_params{GetRandomParams()}
@@ -60,7 +76,8 @@ ExpReciprocal::ExpReciprocal(const GoomRand& goomRand) noexcept
 
 auto ExpReciprocal::GetRandomParams() const noexcept -> Params
 {
-  const auto viewport = m_randomViewport.GetRandomViewport();
+  const auto useRandomViewport = m_goomRand->ProbabilityOf<PROB_USE_RANDOM_VIEWPORT>();
+  const auto viewport = useRandomViewport ? m_randomViewport.GetRandomViewport() : Viewport{};
 
   const auto xAmplitude = m_goomRand->GetRandInRange<AMPLITUDE_RANGE>();
   const auto yAmplitude = m_goomRand->ProbabilityOf<PROB_AMPLITUDES_EQUAL>()
@@ -85,6 +102,10 @@ auto ExpReciprocal::GetRandomParams() const noexcept -> Params
   const auto modulatorPeriod =
       not useModulatorContours ? 0.0F : m_goomRand->GetRandInRange<MODULATOR_PERIOD_RANGE>();
 
+  const auto useMaxAbsSqFz = m_goomRand->ProbabilityOf<PROB_USE_MAX_ABS_SQ_FZ>();
+  const auto maxAbsSqFz =
+      not useMaxAbsSqFz ? MAX_DOUBLE : m_goomRand->GetRandInRange<MAX_ABS_SQ_FZ_RANGE>();
+
   return {
       .viewport               = viewport,
       .amplitude              = {           xAmplitude,            yAmplitude},
@@ -95,6 +116,7 @@ auto ExpReciprocal::GetRandomParams() const noexcept -> Params
       .reciprocalExponent     = reciprocalExponent,
       .useModulatorContours   = useModulatorContours,
       .modulatorPeriod        = modulatorPeriod,
+      .maxAbsSqFz             = maxAbsSqFz,
   };
 }
 
@@ -133,6 +155,10 @@ auto ExpReciprocal::GetVelocity(const NormalizedCoords& coords) const noexcept -
   const auto fz      = std::exp(ONE / std::pow(z, m_params.reciprocalExponent));
   const auto absSqFz = std::norm(fz);
 
+  if (absSqFz > m_params.maxAbsSqFz) // Covers the case: absSqFz = infinity
+  {
+    return {.x = 0.0F, .y = 0.0F};
+  }
   if (absSqFz < SMALL_FLT)
   {
     return {.x = 0.0F, .y = 0.0F};
@@ -177,6 +203,7 @@ auto ExpReciprocal::GetZoomAdjustmentEffectNameValueParams() const noexcept -> N
           "magnify/rotate",
           Point2dFlt{.x = m_params.magnifyAndRotate.real(), .y = m_params.magnifyAndRotate.imag()}),
       GetPair(fullParamGroup, "modulatorPeriod", m_params.modulatorPeriod),
+      GetPair(fullParamGroup, "maxAbsSqFz", m_params.maxAbsSqFz),
       GetPair(PARAM_GROUP,
               "viewport0",
               m_params.viewport
